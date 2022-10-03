29e41a65e5a5eb4230ca09af83eb11c1fd475bc3
Christian <sweatchristian03@gmail.com>
2022-10-03 08:54:10 -0400

    Gradle Initialization Created CaesersCipher class

---
+++ src/main/java/me/christian/caesers_cipher/CaesersCipher.java
@@ -0,0 +1,19 @@
+package me.christian.caesers_cipher;
+
+public class CaesersCipher {
+
+    private String rawInput = "";
+
+    CaesersCipher(String rawInput) {
+        this.rawInput = rawInput;
+    }
+
+    public void encrypt() {
+
+    }
+
+    public void decrypt() {
+
+    }
+
+}
